44ea75c723fd13200e6263b9ddb48605ec098c69
CalicoSpartan <thatcalicospartan@gmail.com>
2019-07-20 00:18:50 -0700

    added background scene to start menu

---
+++ RingDriveCombat/Assets/Scripts/CameraOrbitScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbitScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed;
+
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
